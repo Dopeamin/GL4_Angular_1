8ddb486a8786b697194a4a2cd4c4fd28d4656c47
Mohamed Amine Hamdouni <mohamed@mvst.co>
2022-11-12 10:12:24 +0100

    finishes exercice 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { allIcons, HeroIconModule } from 'ng-heroicon';
 
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HighlightDirective],
   imports: [
+    CommonModule,
     BrowserModule,
     FormsModule,
     HeroIconModule.forRoot(
@@ -21,6 +24,7 @@ import { AppComponent } from './app.component';
     ),
   ],
   providers: [],
+  exports: [HighlightDirective],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
