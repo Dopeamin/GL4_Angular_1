be5683bbc83a0a384b36e4cef0bd712f3a1cbbd1
Mohamed Amine Hamdouni <mohamed@mvst.co>
2022-12-01 21:02:04 +0100

    adds add form

--- src/app/_shared/constants/index.ts
+++ src/app/_shared/constants/index.ts
@@ -3,12 +3,14 @@ import { ColorComponent } from 'src/app/color/color.component';
 import { CvDetailsComponent } from 'src/app/cv/cv-details/cv-details.component';
 import { CvComponent } from 'src/app/cv/cv.component';
 import { FormComponent } from 'src/app/form/form.component';
+import { FormComponent as Form } from 'src/app/carte-visite/form/form.component';
 
 export const ROUTES: Routes = [
   { path: '', component: CvComponent },
   { path: 'cv/details/:id', component: CvDetailsComponent },
   { path: 'color', component: ColorComponent },
   { path: 'authentification', component: FormComponent },
+  { path: 'cv/add', component: Form },
 ];
 
 export const ROUTESLABELS: { path: string; label: string }[] = [
@@ -16,4 +18,5 @@ export const ROUTESLABELS: { path: string; label: string }[] = [
   { path: ROUTES[2].path!, label: 'COLOR' },
   { path: ROUTES[1].path!, label: 'CV' },
   { path: ROUTES[3].path!, label: 'Authentification' },
+  { path: ROUTES[4].path!, label: 'Ajouter Cv' },
 ];
--- src/app/carte-visite/form/form.component.ts
+++ src/app/carte-visite/form/form.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { CvService } from 'src/app/cv-list/cv-service.service';
 import { Carte } from '../carte/carte.model';
 
 @Component({
@@ -9,12 +11,32 @@ import { Carte } from '../carte/carte.model';
 export class FormComponent implements OnInit {
   @Input() carte?: Carte;
   @Output() onClickEvent: EventEmitter<string> = new EventEmitter();
+  isAdd: boolean = true;
   public favoriteColor: string = 'black';
-  constructor() {}
+  constructor(private cvService: CvService, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.carte) this.isAdd = false;
+    this.carte = this.carte || {
+      nom: '',
+      prenom: '',
+      id: this.cvService.getCvs().length,
+      job: '',
+      image: '',
+      couleur: '',
+      citation: '',
+      description: '',
+      keys: '',
+      embauche: false,
+    };
+  }
 
   onClick() {
     this.onClickEvent.emit(this.favoriteColor);
+  }
+
+  onAdd() {
+    this.cvService.addCv(this.carte!);
+    this.router.navigateByUrl('/')
   }
 }
